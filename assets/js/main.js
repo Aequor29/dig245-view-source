067c85aa145f94ba89f2b9d5d28bcbab2ce1071a
Richard Hu <neverlight.hu@gmail.com>
2023-09-12 17:14:19 -0400

    Add License and citation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,7 @@
 
 /* javascript */
+
+console.log("The text is the Socialist Core Value.");
+console.log("《关于培育和践行社会主义核心价值观的意见》 (2013) 中共中央办公厅印发《关于培育和践行社会主义核心价值观的意见》_中央有关文件_中国政府网. Available at: https://www.gov.cn/zhengce/2013-12/23/content_5407875.htm. ")
+console.log("Copyright (c) <2023> <Haoyang (Richard) Hu>");
+
